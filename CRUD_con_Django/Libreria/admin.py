7af8c1bca936043cefdf270cac4d3ba0e2bb4866
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-08 15:54:56 -0500

    Información de los libros a mostrar en el panel de administracion

--- CRUD_con_Django/Libreria/admin.py
+++ CRUD_con_Django/Libreria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Libreria.models import Books
 
-# Register your models here.
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+
+# Registrar los modelos para su administración.
+admin.site.register(Books, BooksAdmin)
