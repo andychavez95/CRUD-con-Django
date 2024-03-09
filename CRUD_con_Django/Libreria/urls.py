b52354fb332268966a337f2fd3a023adfc1fc652
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-08 22:28:38 -0500

    Formulario para guardar libros

--- CRUD_con_Django/Libreria/urls.py
+++ CRUD_con_Django/Libreria/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from Libreria.views import *
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('libros/', list_books, name='list_books'),
     path('libros/crear/', create_book, name='create_book'),
     path('libros/editar/', edit_book, name='edit_book')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- CRUD_con_Django/Libreria/views.py
+++ CRUD_con_Django/Libreria/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from Libreria.models import Books
+from Libreria.forms import BooksForm
 
 def start(request):
     return render(request, 'index.html')
@@ -11,11 +11,17 @@ def us(request):
 
 
 def list_books(request):
-    return render(request, 'books/list_books.html')
+    # Obtenemos todos los libros de la tabla Books.
+    all_books = Books.objects.all()
+    return render(request, 'books/list_books.html', {'books': all_books})
 
 
 def create_book(request):
-    return render(request, 'books/create_book.html')
+    books_form = BooksForm(request.POST or None, request.FILES or None)
+    if books_form.is_valid():
+        books_form.save()
+        return redirect(list_books)
+    return render(request, 'books/create_book.html', {'form': books_form})
 
 def edit_book(request):
     return render(request, 'books/edit_book.html')
